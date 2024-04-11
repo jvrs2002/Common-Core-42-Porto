391503d506beaee9a37d5afe4342bde7e384b97b
Joao Vitor Ribeiro Souza <joao-vri@c1r8s4.42porto.com>
2024-04-11 16:22:30 +0100

    last of the day

--- Projects/Libft/ft_memset.c
+++ Projects/Libft/ft_memset.c
@@ -6,11 +6,11 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:38:38 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/11 16:13:30 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:21:53 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
 
 char    *ft_memset(void *s, int c, size_t n){
     unsigned char    *ptr = s;
