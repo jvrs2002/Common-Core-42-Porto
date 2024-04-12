612326848d98b7b47f01daea77b597d3d740ed32
Joao Vitor Ribeiro Souza <joao-vri@c2r2s3.42porto.com>
2024-04-12 16:50:09 +0100

    atoi and strlcat

--- Projects/Libft/ft_atoi.c
+++ Projects/Libft/ft_atoi.c
@@ -1,40 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 14:14:42 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/12 14:44:52 by joao-vri         ###   ########.fr       */
+/*   Created: 2024/04/12 16:12:22 by joao-vri          #+#    #+#             */
+/*   Updated: 2024/04/12 16:49:37 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
-int	ft_strlcpy(char *dest, const char *src, size_t n)
+ft_atoi(char *src)
 {
-	size_t	i;
+    int i;
+    int signal;
+    int nbr;
 
-	i = 0;
-	while (src[i] != '\0')
-		i++;
-	if (i < n)
-		n = i;
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return i;
+    nbr = 0;
+    signal = 1;
+    i = 0;
+    while ((src[i] >= 9 && src[i] <= 13) || (src[i] == 32))
+        i++;
+    if (src[i] == '-')
+    {
+        signal *= -1;
+        i++;
+    }
+    while (src[i] >= '0' && src[i] <= '9')
+    {
+        nbr = nbr * 10 + src[i] - 48;
+        i++;
+    }
+    return nbr;
 }
-/*int	main(void)
-{
-	char	*src = "hello";
-	char	dest[2];
-	
-	ft_strlcpy(dest, src, 6);
-	printf("%s", dest);
-}*/
--- Projects/Libft/ft_strlcat.c
+++ Projects/Libft/ft_strlcat.c
@@ -1,40 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 14:14:42 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/12 14:44:52 by joao-vri         ###   ########.fr       */
+/*   Created: 2024/04/12 14:49:29 by joao-vri          #+#    #+#             */
+/*   Updated: 2024/04/12 16:04:42 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
-int	ft_strlcpy(char *dest, const char *src, size_t n)
+int ft_strlcat(char *dest, char *src, int n)
 {
-	size_t	i;
+    int i;
 
-	i = 0;
-	while (src[i] != '\0')
-		i++;
-	if (i < n)
-		n = i;
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return i;
+    i = 0;
+    
 }
-/*int	main(void)
-{
-	char	*src = "hello";
-	char	dest[2];
-	
-	ft_strlcpy(dest, src, 6);
-	printf("%s", dest);
-}*/
