388028b658da62b9df311e4b96deec32efaa2f1b
Joao Vitor Ribeiro Souza <joao-vri@c1r11s6.42porto.com>
2024-04-18 16:59:58 +0100

    strnstr finally

--- Projects/Libft/ft_memcpy.c
+++ Projects/Libft/ft_memcpy.c
@@ -1,41 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 14:14:42 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/18 13:05:50 by joao-vri         ###   ########.fr       */
+/*   Created: 2024/04/18 16:43:01 by joao-vri          #+#    #+#             */
+/*   Updated: 2024/04/18 16:59:28 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <string.h>
+#include <stdio.h>
 
-int	ft_strlcpy(char *dst, const char *src, size_t n)
+void	*memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (strlen(src));
 }
-
-/*int	main(void)
-{
-	size_t n = 4;
-	char	*src = "hello";
-	char	dst[n];
-	size_t	i;
-
-	i = ft_strlcpy(dst, src, n);
-	ft_strlcpy(dst, src, n);
-	printf("%s\n", dst);
-	printf("%zu", i);
-}*/
--- Projects/Libft/ft_strnstr.c
+++ Projects/Libft/ft_strnstr.c
@@ -6,40 +6,36 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:18:01 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/17 15:39:13 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/04/18 16:34:32 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	char	*ptr;
 	size_t	i;
 	size_t	n;
 
-	n = -1;
 	i = 0;
-	ptr = NULL;
-	if (little[0] == '\0')
-		return (big);
-	while (little[i] != '\0')
+	if (*little == '\0')
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
 	{
-		n++;
-		i = 0;
-		while (big[n + i] == little[i])
-			i++;
+		n = 0;
+		while (big[i + n] == little[n])
+		{
+			++n;
+			if (little[n] == '\0')
+				return ((char *)&big[i]);
+		}
+		++i;
 	}
-	if (big[n] == '\0')
-		return (NULL);
-	ptr = (char *) big[n];
-	return (ptr);
+	return (NULL);
 }
 
-/*Falta ainda colocar o len como limitador*/
-
-int	main(void)
+/*int	main(void)
 {
-	printf("%s", ft_strnstr("Hello 1 World", "1 World", 20));
-}
+	printf("%s", ft_strnstr("Abctestestextestex", "t", 10));
+}*/
