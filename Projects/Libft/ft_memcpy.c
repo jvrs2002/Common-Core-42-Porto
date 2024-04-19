4e3505a9858a6c8ae10abfd534f3c94b73259700
João Vitor <s.ribeiro.jv@gmail.com>
2024-04-19 09:44:10 +0100

    memcpy now

--- Projects/Libft/ft_memcpy.c
+++ Projects/Libft/ft_memcpy.c
@@ -3,16 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joviribeiro <joviribeiro@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 16:43:01 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/04/18 16:59:28 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/04/19 09:43:46 by joviribeiro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 void	*memcpy(void *dest, const void *src, size_t n)
 {
+	char	*ptr1;
+	size_t	len;
 
+	len = strlen(dest);
+	ptr1 = NULL;
+	while (*src != '\0' && n > 0)
+	{
+		dest[len] = *src;
+		++len;
+		++src;
+		--n;
+	}
+	dest[len] = '\0';
+	ptr1 = (char *) dest;
+	return (ptr1);
 }
+
+
