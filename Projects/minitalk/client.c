77fb92e3ecc40ddc1fb95a2a1d1eb9a8b028c946
Joao Vitor Ribeiro Souza <joao-vri@c1r8s4.42porto.com>
2024-10-14 21:35:20 +0100

    update minitalk

--- Projects/minitalk/client.c
+++ Projects/minitalk/client.c
@@ -6,9 +6,80 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:35:47 by joviribeiro       #+#    #+#             */
-/*   Updated: 2024/10/14 14:46:36 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/10/14 21:05:25 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <limits.h>
 
+static int	g_receiver = 0;
+
+void	signal_handler(int signum, siginfo_t *info, void *context)
+{
+	static int	i = 0;
+
+	(void)info;
+	(void)context;
+	g_receiver = 1;
+	if (signum == SIGUSR2)
+		++i;
+	else if (signum == SIGUSR1)
+	{
+		printf("Number of bytes received: %i", (i / 8));
+		i = 0;
+	}
+
+}
+
+void	send_signal(char c, int pid)
+{
+	size_t	i;
+	size_t	n;
+
+	n = 0;
+	i = CHAR_BIT;
+	while (i-- > 0)
+	{
+		n = 0;
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (g_receiver == 0)
+		{
+			if (n == 50)
+			{
+				printf("No response from server");
+				exit(1);
+			}
+			++n;
+			usleep(100);
+		}
+		g_receiver = 0;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	size_t	i;
+	int		pid;
+	char	*str;
+	struct sigaction_sa;
+
+	if (ac != 3)
+	{
+		printf("ERROR: More than two arguments");
+		return (1);
+	}
+	i = 0;
+	pid = ft_atoi(av[1]);
+	str = av[2];
+	while (str[i] != '\0')
+	{
+		send_signal(str[i], pid);
+		++i;
+	}
+}
--- Projects/minitalk/server.c
+++ Projects/minitalk/server.c
@@ -6,18 +6,32 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:36:39 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/10/14 14:46:35 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:45:11 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
+
+
+// void printCharAsBinaryVer2(char c){
+//     int bit_index;
+//     for (bit_index = __CHAR_BIT__; bit_index >= 0; --bit_index)
+//     {
+//         int bit = ( unsigned int ) (c >> bit_index) & 1;
+//         printf("%d", bit);
+//     }
+// }
+
 int	main(void)
 {
-	printf("%i", getpid()); //This will print the server's PID
-	while (1)
-	{
-		
-	}
+	printf("Server's PID: %i\n", getpid()); //This will print the server's PID
+	// while (1)
+	// {
+
+	// }
+	printf("%i", bin_converter('D'));
+	//printf("%i", bin_converter('d'));
+	// printCharAsBinaryVer2('f');
 }
